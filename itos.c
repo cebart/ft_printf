ddf7561e638435091e3c28140301b53621304fa0
cebart <33933281+cebart@users.noreply.github.com>
2019-04-12 23:25:51 +0200

    Add files via upload

---
+++ itos.c
@@ -0,0 +1,46 @@
+# include "../inc/ft_printf.h"
+
+void    minuscules(char *lettre)
+{
+    int i;
+
+    i = 10;
+    while (lettre[i])
+        lettre[i++] += 32;
+}
+
+int    itos(int base, t_print f)
+{
+    char *buff;
+    char lettre[] = "0123456789ABCDEF";
+    int cmp;
+    int temp;
+    int res;
+
+    cmp = 0;
+    temp = 0;
+    res = 0;
+    buff = ft_strnew(BUFF_SIZE);
+    if(f.nbr < 0)
+    {
+        res++;
+        ft_putchar('-');
+    }
+    else if(f.plus)
+    {
+        res++;
+        ft_putchar('+');
+    }
+    if (f.min != 0)
+        minuscules(lettre);
+    while(f.nbr != 0)
+    {
+        temp = f.nbr % base;
+        temp = (temp < 0) ? -temp : temp;
+        buff[cmp] = lettre[temp];
+        cmp++;
+        f.nbr = f.nbr/base;
+    }
+    print_nbr(buff, &res, f);
+    return (res);
+}
--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 # include "inc/ft_printf.h"
+#include <limits.h>
 
 int main()
 {
     char c = 'c';
     int r = 0;
+    int l = INT_MAX;
     void *v;
     char *s;
 
     v = &c;
     s = ft_strnew(10);
-    r = printf("%% test %p %c %s %+-10d %u %#X %o %#x %#o %s %p %010d %d!\n", s, c, "toto", 10, 1542, 15, 15, 15, 15,"tata", v, 79, -81);
+    r = printf("%% test %p %c %s %+-10d %d %#X %o %#x %#o %s %p %010d %d!\n", s, c, "toto", 10, 569, 15, 15, 15, 15,"tata", v, 79, -81);
     ft_putnbr(r);
     ft_putchar('\n');
     r = 0;
-    r = ft_printf("%% test %p %c %s %+-10d %u %#X %o %#x %#o %s %p %010d %d!\n", s, c, "toto", 10, 1542, 15, 15, 15, 15,"tata", v, 79, -81);
+    r = ft_printf("%% test %p %c %s %+-10d %d %#X %o %#x %#o %s %p %010d %d!\n", s, c, "toto", 10, 569, 15, 15, 15, 15,"tata", v, 79, -81);
     ft_putnbr(r);
     ft_putchar('\n');
     return (0);
 }
+
+
